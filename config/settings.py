e91be8f1c5e142e1c74ff301757c51c46e4eb2b6
iftix0r <iftixortoychiyev1@gmail.com>
2026-07-16 12:23:18 +0500

    feat: update database settings to use PostgreSQL and add psycopg2-binary dependency

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'host7905_vijdon1515',
+        'USER': 'host7905_vijdon_user',
+        'PASSWORD': 'hkT+sFnBLp.e&Rxi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
